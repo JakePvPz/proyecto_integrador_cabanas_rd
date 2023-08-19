abd851f0e43a1347f8d2bd19e4a0c7ada264aa98
JakePvPz <44032238+JakePvPz@users.noreply.github.com>
2023-08-19 14:42:15 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -27,9 +27,17 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
-app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 app.use("/downloads", express.static("downloads"));
 
